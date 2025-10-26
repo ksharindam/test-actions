a64d0bd2ee1e33705e858b3c50db6fe61da5aa6c
Arindam Chaudhuri <ksharindam@gmail.com>
2025-10-26 16:47:12 +0530

    debian packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
+from setuptools.command.install import install
 try:
     from setuptools.command.bdist_wheel import bdist_wheel
 except:
     from wheel.bdist_wheel import bdist_wheel
 from subprocess import check_call
 import platform
+
+# allows to run commands before 'setup.py install'
+class Install(install):
+    def run(self):
+        check_call("pyrcc5 -o ./chemcanvas/resources_rc.py ./data/resources.qrc".split())
+        check_call("pyuic5 -o ./chemcanvas/ui_mainwindow.py ./data/mainwindow.ui".split())
+        install.run(self)
 
 # allows to run commands before building wheel
 class BdistWheel(bdist_wheel):
@@ -16,7 +24,7 @@ class BdistWheel(bdist_wheel):
 
 if platform.system()=='Linux':
     data_files = [('share/applications', ['data/chemcanvas.desktop']),
-                ('share/icons', ['data/chemcanvas.svg'])]
+                ('share/icons/hicolor/scalable/apps', ['data/chemcanvas.svg'])]
 else:
     data_files = []
 
